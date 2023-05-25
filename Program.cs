54f71c8348fe131e549176f4e4ebdc3872cd817e
karpekin <karpekin@mail.ru>
2023-05-25 07:30:40 +0300

    task 60

--- Program.cs
+++ Program.cs
@@ -140,45 +140,110 @@
 // int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
 // int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
-int[,] martrix1 = new int[,]{{2,4},
-                             {3,2}};
-PrintArray(martrix1);
-Console.WriteLine("x");
+// int[,] martrix1 = new int[,]{{2,4},
+//                              {3,2}};
+// PrintArray(martrix1);
+// Console.WriteLine("x");
 
-int[,] martrix2 = new int[,]{{3,4},
-                             {3,3}};
-PrintArray(martrix2);
+// int[,] martrix2 = new int[,]{{3,4},
+//                              {3,3}};
+// PrintArray(martrix2);
 
-int[,] sum_matrix = new int[2,2];
+// int[,] sum_matrix = new int[2,2];
 
-SumMatrix(martrix1, martrix2, sum_matrix);
-Console.WriteLine("______________________");
-PrintArray(sum_matrix);
+// SumMatrix(martrix1, martrix2, sum_matrix);
+// Console.WriteLine("______________________");
+// PrintArray(sum_matrix);
 
-void SumMatrix(int[,] matrix1, int[,] martrix2, int[,] sum_matrix)
+// void SumMatrix(int[,] matrix1, int[,] martrix2, int[,] sum_matrix)
+// {
+//   for (int i = 0; i < sum_matrix.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < sum_matrix.GetLength(1); j++)
+//     {
+//       int sum = 0;
+//       for (int k = 0; k < matrix1.GetLength(1); k++)
+//       {
+//         sum += martrix1[i,k] * martrix2[k,j];
+//       }
+//       sum_matrix[i,j] = sum;
+//     }
+//   }
+// }
+
+// void PrintArray (int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i,j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.WriteLine("Введите размеры массива через пробел: ");
+string[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,,] array = GetArray(new int[] { int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), }, 10, 99);
+PrintArray(array);
+
+int[,,] GetArray(int[] sizes, int min, int max)
 {
-  for (int i = 0; i < sum_matrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < sum_matrix.GetLength(1); j++)
+    int[,,] result = new int[sizes[0], sizes[1], sizes[2]];
+    for (int i = 0; i < result.GetLength(0); i++)
     {
-      int sum = 0;
-      for (int k = 0; k < matrix1.GetLength(1); k++)
-      {
-        sum += martrix1[i,k] * martrix2[k,j];
-      }
-      sum_matrix[i,j] = sum;
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            int k = 0;
+            while (k < result.GetLength(2))
+            {
+                int element = new Random().Next(min, max + 1);
+                if (FindElement(result, element)) continue;
+                result[i, j, k] = element;
+                k++;
+            }
+        }
     }
-  }
+    return result;
 }
-
-void PrintArray (int[,] array)
+bool FindElement(int[,,] array, int element)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-      Console.Write(array[i,j] + " ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == element) return true;
+            }
+        }
     }
-    Console.WriteLine();
-  }
+    return false;
 }
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
+  
